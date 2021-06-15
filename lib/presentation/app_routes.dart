aee528175f7b4c57af4070c4ed3d1a071c7aef8a
Azhar <azharuddin.koya@kotak.com>
2021-06-15 12:21:24 +0530

    basic setup 4

--- lib/presentation/app_routes.dart
+++ lib/presentation/app_routes.dart
@@ -14,9 +14,6 @@ class RouteList {
 
   static const String initial = '/';
   static const String home = '/home';
-  static const String movieDetail = '/movie-detail';
-  static const String watchTrailer = '/watch-trailer';
-  static const String favorite = '/favorite';
 }
 
 appRoutes(RouteSettings settings) {
